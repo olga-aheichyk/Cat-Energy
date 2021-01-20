1d5926a1fd876ae433744b751c05066111c89f5d
olga.aheichyk <olga.aheichyk@gmail.com>
2021-01-20 14:15:53 +0300

    make pixel-perfect of index mobile

--- source/js/script.js
+++ source/js/script.js
@@ -4,21 +4,3 @@ var navigationMobile = document.querySelector(".navigation");
 toggleButton.addEventListener ("click", function() {
   navigationMobile.classList.toggle("navigation--opened");
 });
-
-
-/*
-function initMap() {
-  var coordinates = {lat: 59.938744880956804, lng: 30.32296241647558},
-
-      map = new google.maps.Map(document.querySelector(".location__map"), {
-          center: coordinates
-      });
-
-      image = "img/map-pin-mobile.png",
-      marker = new google.maps.Marker({
-        position: coordinates,
-        map: map,
-        icon: image
-    });
-}
-*/
