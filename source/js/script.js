f1ff5bb92e6a626dc006bce2863a86eafee5645d
olga.aheichyk <olga.aheichyk@gmail.com>
2021-01-21 15:13:50 +0300

    make pixel-perfect of index-desktop

--- source/js/script.js
+++ source/js/script.js
@@ -1,5 +1,8 @@
+var navigationNoJS = document.querySelector(".navigation--no-js");
 var toggleButton = document.querySelector(".navigation__toggle");
 var navigationMobile = document.querySelector(".navigation");
+
+navigationNoJS.classList.remove("navigation--no-js");
 
 toggleButton.addEventListener ("click", function() {
   navigationMobile.classList.toggle("navigation--opened");
